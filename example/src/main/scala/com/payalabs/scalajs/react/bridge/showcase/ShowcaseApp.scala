9de0d302180efc44fbebc5b17d9e4040bb905cae
Ramnivas Laddad <ramnivas@ramnivas.com>
2017-07-21 09:03:49 -0700

    Make Tags component actually update the state

--- example/src/main/scala/com/payalabs/scalajs/react/bridge/showcase/ShowcaseApp.scala
+++ example/src/main/scala/com/payalabs/scalajs/react/bridge/showcase/ShowcaseApp.scala
@@ -1,7 +1,7 @@
 package com.payalabs.scalajs.react.bridge.showcase
 
-import com.payalabs.scalajs.react.bridge.elements.{ReactMediumEditor, FormControl, Button, TagsInput}
-import japgolly.scalajs.react.vdom.all.{onChange => _,_}
+import com.payalabs.scalajs.react.bridge.elements.{Button, FormControl, ReactMediumEditor, TagsInput}
+import japgolly.scalajs.react.vdom.all._
 import japgolly.scalajs.react._
 import org.scalajs.dom
 
@@ -9,38 +9,57 @@ import scala.scalajs.js
 
 object ShowcaseApp extends js.JSApp {
   def main(): Unit = {
+    ShowcaseComponent().renderIntoDOM(dom.document.getElementById("app-container"))
+  }
+}
 
-    def printSequence(value: js.Array[String]): Callback = Callback.log(s"Value is ${value.mkString(",")}")
+object ShowcaseComponent {
+  case class State(tags: Seq[String])
+  type Props = Unit
+
+  class Backend(scope: BackendScope[Props, State]) {
+    def updateTags(value: js.Array[String]) = scope.modState(s =>
+      s.copy(tags = value.toSeq)
+    )
+
     def validateLength(value: String) = CallbackTo { value.length <= 7 }
 
-    val component = ScalaComponent.builder[Unit]("ShowcaseApp").render { _ =>
+    def render(state: State): VdomElement = {
       div(className := "col-sm-10 col-sm-offset-1")(
         h1()(
           "Example components created with ",
           a(href := "https://github.com/payalabs/scalajs-react-bridge")("scalajs-react-bridge")
         ),
         div(className := "well")(
-          b("Tags input (open Inspect Element to see callback being called as you make changes). Values with size > 7 considered invalid"),
-          TagsInput(value = Seq("some", "default", "values"), onChange = printSequence _, validate = validateLength _)
+          b("Tags input. Values with size > 7 considered invalid"),
+          TagsInput(value = state.tags, onChange = updateTags _, validate = validateLength _)
         ),
         div(className := "well")(
           ReactMediumEditor(text =
             """
-            | <h1>Medium Editor</h1>
-            |
-            | <p>Click here and start editing.</p>
-            |
-            | <b>Select some text to see the editor toolbar pop up.</b>
+              | <h1>Medium Editor</h1>
+              |
+              | <p>Click here and start editing.</p>
+              |
+              | <b>Select some text to see the editor toolbar pop up.</b>
             """.stripMargin, options = Map("buttons" -> js.Array("bold", "italic", "underline", "anchor", "header1", "header2", "quote", "orderedlist", "unorderedlist")))
         ),
         div(className := "well")(
           b("Bootstrap"),
           FormControl(placeholder = "This is a bootstrap input", `type` = "text"),
-          Button().apply("Bootstrap Button")
+          Button()("Bootstrap Button")
         )
       )
-    }.build.apply()
 
-    component.renderIntoDOM(dom.document.getElementById("app-container"))
+    }
   }
+
+  val component =
+    ScalaComponent.builder[Props](this.getClass.getSimpleName).
+      initialState(State(tags = Seq("some", "default", "tags"))).
+      renderBackend[Backend].
+      build
+
+  def apply(): VdomElement = component()
+
 }
