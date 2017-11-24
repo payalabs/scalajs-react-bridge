e7f9eb67713038261672d9600fe5f7191575d61d
Ramnivas Laddad <ramnivas@ramnivas.com>
2017-11-24 12:28:13 -0800

    Specify the return type in GenericComponent
    Without the return type (more accurately, without the import
    for `WithProps`) the code doesn't compile.

--- core/src/test/scala/com/payalabs/scalajs/react/bridge/test/GenericComponent.scala
+++ core/src/test/scala/com/payalabs/scalajs/react/bridge/test/GenericComponent.scala
@@ -1,13 +1,12 @@
 package com.payalabs.scalajs.react.bridge.test
 
 import scala.scalajs.js
-
-import com.payalabs.scalajs.react.bridge.ReactBridgeComponent
+import com.payalabs.scalajs.react.bridge.{ReactBridgeComponent, WithProps}
 
 
 /**
   * This is here to "test" (at compile-time) that ReactComponentBridge works with generics
   */
 object GenericComponent extends ReactBridgeComponent {
-  def apply[A <: js.Any](param: A): Any = auto
+  def apply[A <: js.Any](param: A): WithProps = auto
 }
